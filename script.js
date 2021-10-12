0631a3ba081a79ed2f57290847f23386b67444a0
net234 <51489394+net234@users.noreply.github.com>
2021-10-12 02:41:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function doGet(parametres) {
   try {
     var localTime = new Date();  // local Time in excel format
     var timestamp = unixLocalTime(localTime);   // local time in unix format
-    var timezone = localTime.getTimezoneOffset()/60;
+    var timezone = localTime.getTimezoneOffset() / 60;
 
     var node = parametres.parameter.node;       // identificateur unique de la device emetrice ex : node01
     if (!node) throw 'bad device';  // doesnt use node 
@@ -31,34 +31,34 @@ function doGet(parametres) {
   //values[1][1] = excelLocalDate(); // signale la date de lecture
   //range.setValues(values);
 
-    var eventJSON = {
-      'status': true,
-      'message': 'Success',
-      'action': action,
-      'timestamp': timestamp,
-      'timezone' : timezone,
-      'baseindex': baseIndex,
-    }
+  var eventJSON = {
+    'status': true,
+    'message': 'Ok',
+    'action': action,
+    'timestamp': timestamp,
+    'timezone': timezone,
+    'baseindex': baseIndex,
+  }
 
   if (action == 'check') {
     sheet.getRange(newRow, 5).setValue('Ok');
     return ContentService.createTextOutput(JSON.stringify(eventJSON)).setMimeType(ContentService.MimeType.JSON);
   }
-  if (action == 'mark') {
-     var range = SpreadsheetApp.getActiveSpreadsheet().getRangeByName('BaseInfo');
-     var values = range.getValues();
-     values[1][0]=values[0][0];
-     range.setValues(values);
-    
-    sheet.getRange(newRow, 5).setValue('Ok');
-    return ContentService.createTextOutput(JSON.stringify(eventJSON)).setMimeType(ContentService.MimeType.JSON);
-  }
+  //if (action == 'mark') {
+  //   var range = SpreadsheetApp.getActiveSpreadsheet().getRangeByName('BaseInfo');
+  //   var values = range.getValues();
+  //   values[1][0]=values[0][0];
+  //   range.setValues(values);
+  //  
+  //  sheet.getRange(newRow, 5).setValue('Ok');
+  //  return ContentService.createTextOutput(JSON.stringify(eventJSON)).setMimeType(ContentService.MimeType.JSON);
+  //}
 
 
   if (action == 'getBaseInfo') {
     result = SpreadsheetApp.getActiveSpreadsheet().getRangeByName('BaseInfo').getValues();
-    result[0][1]=unixLocalTime(result[0][1]); 
-    result[1][1]=unixLocalTime(result[1][1]);
+    result[0][1] = unixLocalTime(result[0][1]);
+    result[1][1] = unixLocalTime(result[1][1]);
     eventJSON.answer = result;
     sheet.getRange(newRow, 5).setValue('Ok');
     return ContentService.createTextOutput(JSON.stringify(eventJSON)).setMimeType(ContentService.MimeType.JSON);
@@ -73,15 +73,53 @@ function doGet(parametres) {
   }
 
   if (action == 'getBadges') {
-    
-    sheetBadges = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Badges');
-    numberBadges = sheetBadges.getLastRow()-1;
-    result = sheetBadges.getRange(2,1,numberBadges,6).getValues();
-    // conversion des dates
-    for (N = 0 ; N < numberBadges ; N++) {
-      result[N][2] = unixLocalTime(result[N][2]);
-      result[N][3] = unixLocalTime(result[N][3]); 
+    // get max(=10) badges from first(=1)
+    var paramJson = parametres.parameter.json;        // first=xxxx  max=xxxx
+    var first = 1;
+    var max = 10;
+    // get obtional parameters
+    if (paramJson) {
+      var params = JSON.parse(paramJson);
+      first = params.first;
+      max = params.max;
+      if (max < 1) max = 10;
     }
+
+    if (max < 1) max = 1;
+    if (first < 1) first = 1;
+
+    //marque la version en cours de lecture uniquement si on est en debut de lecture
+    var range = SpreadsheetApp.getActiveSpreadsheet().getRangeByName('BaseInfo');
+    var values = range.getValues();
+    var baseIndex = values[0][0];
+    if (first == 1) {
+      values[1][0] = baseIndex;
+      range.setValues(values);
+    }
+
+
+    sheetBadges = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Badges');
+    totalBadges = sheetBadges.getLastRow() - 1;
+    var len = max;
+    if (first - 1 + len > totalBadges) len = totalBadges - first + 1;
+    var badges;
+    if (len > 0) {
+      badges = sheetBadges.getRange(first + 1, 1, len, 6).getValues();
+      // conversion des dates en day from 2000  (make json string much shorter)
+      for (N = 0; N < len; N++) {
+        badges[N][2] = unixDaysFrom2000(badges[N][2]);
+        badges[N][3] = unixDaysFrom2000(badges[N][3]);
+      }
+    }
+    var result = {
+      'baseindex': baseIndex,
+      'first': first,
+      'len': len,
+      'total': totalBadges,
+      'eof': (first - 1 + len >= totalBadges),
+      'badges': badges,
+    }
+
     eventJSON.answer = result;
     sheet.getRange(newRow, 5).setValue('Ok');
     return ContentService.createTextOutput(JSON.stringify(eventJSON)).setMimeType(ContentService.MimeType.JSON);
@@ -93,17 +131,17 @@ function doGet(parametres) {
   if (action == 'writeInfo') {
     //result = SpreadsheetApp.getActiveSpreadsheet().getRangeByName('PlagesHoraire').getValues();;
     var paramJson = parametres.parameter.json;        // nom de l'evenerment :  ex BP0
-    if (!paramJson ) {  
-      eventJSON.status =  false,
-      event.JSON.message = 'Error no param info'  
+    if (!paramJson) {
+      eventJSON.status = false,
+        event.JSON.message = 'Error no param info'
     } else {
       var params = JSON.parse(paramJson);
-      sheet.getRange(newRow, 6).setValue(params.info);  
-    }  
+      sheet.getRange(newRow, 6).setValue(params.info);
+    }
     sheet.getRange(newRow, 5).setValue('Ok');
     return ContentService.createTextOutput(JSON.stringify(eventJSON)).setMimeType(ContentService.MimeType.JSON);
   }
-  
+
 
 
   var id = parametres.parameter.ID;
@@ -136,8 +174,10 @@ function doGet(parametres) {
     for (var i = 0; i < lastRow2; i++) {
       if (values[i][0] == key) {
         var result = values[i];
-        result[2] = new Date(result[2]) / 1000;
-        result[3] = new Date(result[3]) / 1000;
+        // make short date delta in days from 1/1/2000
+        startDate = new Date(2020, 12, 1)
+        result[2] = (new Date(result[2]) - startDate) / (3600 * 1000);
+        result[3] = (new Date(result[3]) - startDate) / (3600 * 1000);
         break;
       }
     }
@@ -148,7 +188,7 @@ function doGet(parametres) {
       result = ['Not found'];
       sheet.getRange(newRow, 5).setValue('Not found');
     }
-    
+
     var eventJSON = {
       'status': true,
       'message': 'Success',
@@ -162,7 +202,7 @@ function doGet(parametres) {
     return ContentService.createTextOutput(JSON.stringify(eventJSON)).setMimeType(ContentService.MimeType.JSON);
   }
 
-    sheet.getRange(newRow, 5).setValue('Err action : ' + action);
+  sheet.getRange(newRow, 5).setValue('Err action : ' + action);
 
   var eventJSON = {
     'status': false,
@@ -201,7 +241,10 @@ function unixLocalTime(aExeclDate) {
   return Math.floor(aExeclDate.getTime() / 1000) - aExeclDate.getTimezoneOffset() * 60;
 }
 
-
+function unixDaysFrom2000(aExeclDate) {
+  if (!aExeclDate) aExeclDate = new Date();
+  return (aExeclDate.getTime() - new Date(2000, 12, 1).getTime()) / (1000 * 3600 * 24);
+}
 
 
 // timestamp unix : nombre de secondes depuis 1970
